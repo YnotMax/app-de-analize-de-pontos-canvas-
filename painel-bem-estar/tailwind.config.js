e3b2b9cf1a4a98086858aaf121e679148626f87e
tony max <tonymaxonline@gmail.com>
2025-12-04 12:29:29 -0300

    .

--- painel-bem-estar/tailwind.config.js
+++ painel-bem-estar/tailwind.config.js
@@ -8,11 +8,11 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: 'rgba(var(--color-primary), <alpha-value>)',
-        secondary: 'rgba(var(--color-secondary), <alpha-value>)',
-        accent: 'rgba(var(--color-accent), <alpha-value>)',
-        'background-light': 'rgba(var(--color-background-light), <alpha-value>)',
-        'background-dark': 'rgba(var(--color-background-dark), <alpha-value>)',
+        primary: 'rgb(var(--color-primary) / <alpha-value>)',
+        secondary: 'rgb(var(--color-secondary) / <alpha-value>)',
+        accent: 'rgb(var(--color-accent) / <alpha-value>)',
+        'background-light': 'rgb(var(--color-background-light) / <alpha-value>)',
+        'background-dark': 'rgb(var(--color-background-dark) / <alpha-value>)',
       },
       fontFamily: {
         display: ['Inter', 'sans-serif'],
